8262283add8123fcd0952ec91601e430b95c9568
brunommartins22 <50213278+brunommartins22@users.noreply.github.com>
2019-05-06 18:01:39 -0300

    create classes cliente e produto

---
+++ src/main/java/br/com/interagese/syscontabil/models/Cliente.java
@@ -0,0 +1,346 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.interagese.syscontabil.models;
+
+import br.com.interagese.padrao.rest.models.Usuario;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Bruno Martins
+ */
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen_cli")
+    @SequenceGenerator(name = "gen_cli", sequenceName = "seq_cli")
+    private Long id;
+    private String nmCliente;
+    private String email;
+    private String nmFantasia;
+    private String tpCliente;
+    private String cpfCnpj;
+    private String rgIe;
+    private String endereco;
+    private String numero;
+    private String bairro;
+    private String pais;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String perimetro;
+    private String fone1;
+    private String fone2;
+    @OneToOne
+    private Usuario usuarioRegistrador;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataRegistro;
+
+    //*********************** Eequals && Hashcode ******************************
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.getId());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.interagese.syscontabil.models.Cliente { id=" + getId() + " ]";
+    }
+
+    //*************************** get && setts *********************************
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nmCliente
+     */
+    public String getNmCliente() {
+        return nmCliente;
+    }
+
+    /**
+     * @param nmCliente the nmCliente to set
+     */
+    public void setNmCliente(String nmCliente) {
+        this.nmCliente = nmCliente;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the nmFantasia
+     */
+    public String getNmFantasia() {
+        return nmFantasia;
+    }
+
+    /**
+     * @param nmFantasia the nmFantasia to set
+     */
+    public void setNmFantasia(String nmFantasia) {
+        this.nmFantasia = nmFantasia;
+    }
+
+    /**
+     * @return the tpCliente
+     */
+    public String getTpCliente() {
+        return tpCliente;
+    }
+
+    /**
+     * @param tpCliente the tpCliente to set
+     */
+    public void setTpCliente(String tpCliente) {
+        this.tpCliente = tpCliente;
+    }
+
+    /**
+     * @return the cpfCnpj
+     */
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    /**
+     * @param cpfCnpj the cpfCnpj to set
+     */
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    /**
+     * @return the rgIe
+     */
+    public String getRgIe() {
+        return rgIe;
+    }
+
+    /**
+     * @param rgIe the rgIe to set
+     */
+    public void setRgIe(String rgIe) {
+        this.rgIe = rgIe;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    /**
+     * @return the pais
+     */
+    public String getPais() {
+        return pais;
+    }
+
+    /**
+     * @param pais the pais to set
+     */
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the perimetro
+     */
+    public String getPerimetro() {
+        return perimetro;
+    }
+
+    /**
+     * @param perimetro the perimetro to set
+     */
+    public void setPerimetro(String perimetro) {
+        this.perimetro = perimetro;
+    }
+
+    /**
+     * @return the fone1
+     */
+    public String getFone1() {
+        return fone1;
+    }
+
+    /**
+     * @param fone1 the fone1 to set
+     */
+    public void setFone1(String fone1) {
+        this.fone1 = fone1;
+    }
+
+    /**
+     * @return the fone2
+     */
+    public String getFone2() {
+        return fone2;
+    }
+
+    /**
+     * @param fone2 the fone2 to set
+     */
+    public void setFone2(String fone2) {
+        this.fone2 = fone2;
+    }
+
+    /**
+     * @return the usuarioRegistrador
+     */
+    public Usuario getUsuarioRegistrador() {
+        return usuarioRegistrador;
+    }
+
+    /**
+     * @param usuarioRegistrador the usuarioRegistrador to set
+     */
+    public void setUsuarioRegistrador(Usuario usuarioRegistrador) {
+        this.usuarioRegistrador = usuarioRegistrador;
+    }
+
+    /**
+     * @return the dataRegistro
+     */
+    public Date getDataRegistro() {
+        return dataRegistro;
+    }
+
+    /**
+     * @param dataRegistro the dataRegistro to set
+     */
+    public void setDataRegistro(Date dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
+}
--- src/main/java/br/com/interagese/syscontabil/models/Produto.java
+++ src/main/java/br/com/interagese/syscontabil/models/Produto.java
@@ -5,39 +5,46 @@
  */
 package br.com.interagese.syscontabil.models;
 
+import br.com.interagese.padrao.rest.models.Usuario;
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
  * @author adam
  */
 @Entity
-@Table(name = "tabpro")
+@Table(name = "produto")
 public class Produto implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen_tabpro")
-    @SequenceGenerator(name = "gen_tabpro", sequenceName = "seq_tabpro")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen_prod")
+    @SequenceGenerator(name = "gen_prod", sequenceName = "seq_prod")
     private Long id;
+    private String nmProduto;
+    private String ean;
+    private String ncm;
+    private String cest;
+    private String unidade;
+    @OneToOne
+    private Usuario usuarioRegistrador;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataRegistro;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    //************************** Equals && hashcode ****************************
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (getId() != null ? getId().hashCode() : 0);
         return hash;
     }
 
@@ -48,7 +55,7 @@ public class Produto implements Serializable {
             return false;
         }
         Produto other = (Produto) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.getId() == null && other.getId() != null) || (this.getId() != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
@@ -56,7 +63,114 @@ public class Produto implements Serializable {
 
     @Override
     public String toString() {
-        return "br.com.interagese.siscontabil.models.Produto[ id=" + id + " ]";
+        return "br.com.interagese.syscontabil.models.Produto[ id=" + getId() + " ]";
+    }
+
+    //******************************* get && setts *****************************
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nmProduto
+     */
+    public String getNmProduto() {
+        return nmProduto;
+    }
+
+    /**
+     * @param nmProduto the nmProduto to set
+     */
+    public void setNmProduto(String nmProduto) {
+        this.nmProduto = nmProduto;
+    }
+
+    /**
+     * @return the ean
+     */
+    public String getEan() {
+        return ean;
+    }
+
+    /**
+     * @param ean the ean to set
+     */
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
+    /**
+     * @return the ncm
+     */
+    public String getNcm() {
+        return ncm;
+    }
+
+    /**
+     * @param ncm the ncm to set
+     */
+    public void setNcm(String ncm) {
+        this.ncm = ncm;
+    }
+
+    /**
+     * @return the cest
+     */
+    public String getCest() {
+        return cest;
+    }
+
+    /**
+     * @param cest the cest to set
+     */
+    public void setCest(String cest) {
+        this.cest = cest;
+    }
+
+    /**
+     * @return the unidade
+     */
+    public String getUnidade() {
+        return unidade;
+    }
+
+    /**
+     * @param unidade the unidade to set
+     */
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    /**
+     * @return the usuarioRegistrador
+     */
+    public Usuario getUsuarioRegistrador() {
+        return usuarioRegistrador;
+    }
+
+    /**
+     * @param usuarioRegistrador the usuarioRegistrador to set
+     */
+    public void setUsuarioRegistrador(Usuario usuarioRegistrador) {
+        this.usuarioRegistrador = usuarioRegistrador;
+    }
+
+    /**
+     * @return the dataRegistro
+     */
+    public Date getDataRegistro() {
+        return dataRegistro;
+    }
+
+    /**
+     * @param dataRegistro the dataRegistro to set
+     */
+    public void setDataRegistro(Date dataRegistro) {
+        this.dataRegistro = dataRegistro;
     }
 
 }
