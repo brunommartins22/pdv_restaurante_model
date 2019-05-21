6372e7be67c03ead7b4b5bc1c2324eac654c6b75
josev.junior <josev.junior@outlook.com>
2019-05-21 18:14:17 -0300

    Alterações:  - Integração com liquibase

--- src/main/java/br/com/interagese/CrudApplication.java
+++ src/main/java/br/com/interagese/CrudApplication.java
@@ -1,12 +1,53 @@
 package br.com.interagese;
 
+import javax.sql.DataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class CrudApplication {
+    
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public LiquibaseProperties liquibaseProperties() {
+        return new LiquibaseProperties();
+    }
+
+    @Bean
+    @DependsOn(value = "entityManagerFactory")
+    public SpringLiquibase liquibase() {
+        LiquibaseProperties liquibaseProperties = liquibaseProperties();
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDataSource(getDataSource(liquibaseProperties));
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setShouldRun(true);
+        liquibase.setLabels(liquibaseProperties.getLabels());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        return liquibase;
+    }
+
+    private DataSource getDataSource(LiquibaseProperties liquibaseProperties) {
+        if (liquibaseProperties.getUrl() == null) {
+            return this.dataSource;
+        }
+        return DataSourceBuilder.create().url(liquibaseProperties.getUrl())
+            .username(liquibaseProperties.getUser())
+            .password(liquibaseProperties.getPassword()).build();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(CrudApplication.class, args);
