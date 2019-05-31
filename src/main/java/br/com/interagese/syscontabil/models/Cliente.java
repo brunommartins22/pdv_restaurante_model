2d68f8123847c457d20ea3cf86cab312b4bf09c3
renatosmpa <renatomaiandeua@gmail.com>
2019-05-31 16:28:02 -0300

    Update Cliente.java
    model

--- src/main/java/br/com/interagese/syscontabil/models/Cliente.java
+++ src/main/java/br/com/interagese/syscontabil/models/Cliente.java
@@ -6,6 +6,7 @@
 package br.com.interagese.syscontabil.models;
 
 import br.com.interagese.erplibrary.AtributoPadrao;
+import br.com.interagese.erplibrary.EnderecoPadrao;
 import br.com.interagese.padrao.rest.models.Cidade;
 import br.com.interagese.padrao.rest.models.Estado;
 import br.com.interagese.rest.domain.DominioTipoPessoa;
@@ -44,46 +45,21 @@ public class Cliente implements Serializable {
     private String nomeFantasia;
     @Column(length = 255)
     private String nomeResposavel;
+    
     @Enumerated(EnumType.STRING)
     private DominioRegime tipoRegime;
+    
+    @Column( length = 30)
     @Enumerated(EnumType.STRING)
-    private DominioTipoPessoa tipoCliente;
-    @Column(length = 14, nullable = false, unique = true)
-    private String cpfCnpj;
-    @Column(length = 12, nullable = false, unique = true)
-    private String rgIe;
-    @Column(length = 12, unique = true)
-    private String im;
-    @Column(length = 12, unique = true)
-    private String ieSt;
-    @Column(length = 12, unique = true)
-    private String suframa;
-    @Column(length = 12, nullable = false)
-    private String crt;
-    @Column(length = 8)
-    private String cep;
-    @Column(length = 255)
-    private String logradouro;
-    @Column(length = 120)
-    private String numero;
-    @Column(length = 255)
-    private String complemento;
-    @Column(length = 120)
-    private String bairro;
-    @ManyToOne
-    @JoinColumn(foreignKey = @ForeignKey(name = "cliente_fk_estado"))
-    private Estado estado;
-    @ManyToOne
-    @JoinColumn(foreignKey = @ForeignKey(name = "cliente_fk_cidade"))
-    private Cidade cidade;
-    @Column(length = 12)
-    private String fone1;
-    @Column(length = 12)
-    private String fone2;
-    @Column(length = 120)
-    private String email;
+    private DominioTipoPessoa tipoPessoa;
+    
     @Embedded
-    private AtributoPadrao atributoPadrao = new AtributoPadrao();
+    private EnderecoPadrao endereco;
+    
+    @Embedded
+    private AtributoPadrao atributoPadrao;
+    
+    
 
     public Cliente() {
 
@@ -121,6 +97,7 @@ public class Cliente implements Serializable {
     }
 
     //*************************** get && setts *********************************
+
     public Long getId() {
         return id;
     }
@@ -129,312 +106,61 @@ public class Cliente implements Serializable {
         this.id = id;
     }
 
-    /**
-     * @return the razaoSocial
-     */
     public String getRazaoSocial() {
         return razaoSocial;
     }
 
-    /**
-     * @param razaoSocial the razaoSocial to set
-     */
     public void setRazaoSocial(String razaoSocial) {
         this.razaoSocial = razaoSocial;
     }
 
-    /**
-     * @return the nomeFantasia
-     */
     public String getNomeFantasia() {
         return nomeFantasia;
     }
 
-    /**
-     * @param nomeFantasia the nomeFantasia to set
-     */
     public void setNomeFantasia(String nomeFantasia) {
         this.nomeFantasia = nomeFantasia;
     }
 
-    /**
-     * @return the nomeResposavel
-     */
     public String getNomeResposavel() {
         return nomeResposavel;
     }
 
-    /**
-     * @param nomeResposavel the nomeResposavel to set
-     */
     public void setNomeResposavel(String nomeResposavel) {
         this.nomeResposavel = nomeResposavel;
     }
 
-    /**
-     * @return the tipoCliente
-     */
-    public DominioTipoPessoa getTipoCliente() {
-        return tipoCliente;
-    }
-
-    /**
-     * @param tipoCliente the tipoCliente to set
-     */
-    public void setTipoCliente(DominioTipoPessoa tipoCliente) {
-        this.tipoCliente = tipoCliente;
-    }
-
-    /**
-     * @return the cpfCnpj
-     */
-    public String getCpfCnpj() {
-        return cpfCnpj;
-    }
-
-    /**
-     * @param cpfCnpj the cpfCnpj to set
-     */
-    public void setCpfCnpj(String cpfCnpj) {
-        this.cpfCnpj = cpfCnpj;
-    }
-
-    /**
-     * @return the rgIe
-     */
-    public String getRgIe() {
-        return rgIe;
-    }
-
-    /**
-     * @param rgIe the rgIe to set
-     */
-    public void setRgIe(String rgIe) {
-        this.rgIe = rgIe;
-    }
-
-    /**
-     * @return the im
-     */
-    public String getIm() {
-        return im;
-    }
-
-    /**
-     * @param im the im to set
-     */
-    public void setIm(String im) {
-        this.im = im;
-    }
-
-    /**
-     * @return the ieSt
-     */
-    public String getIeSt() {
-        return ieSt;
-    }
-
-    /**
-     * @param ieSt the ieSt to set
-     */
-    public void setIeSt(String ieSt) {
-        this.ieSt = ieSt;
-    }
-
-    /**
-     * @return the suframa
-     */
-    public String getSuframa() {
-        return suframa;
-    }
-
-    /**
-     * @param suframa the suframa to set
-     */
-    public void setSuframa(String suframa) {
-        this.suframa = suframa;
-    }
-
-    /**
-     * @return the crt
-     */
-    public String getCrt() {
-        return crt;
-    }
-
-    /**
-     * @param crt the crt to set
-     */
-    public void setCrt(String crt) {
-        this.crt = crt;
-    }
-
-    /**
-     * @return the cep
-     */
-    public String getCep() {
-        return cep;
-    }
-
-    /**
-     * @param cep the cep to set
-     */
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    /**
-     * @return the logradouro
-     */
-    public String getLogradouro() {
-        return logradouro;
-    }
-
-    /**
-     * @param logradouro the logradouro to set
-     */
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
-    }
-
-    /**
-     * @return the numero
-     */
-    public String getNumero() {
-        return numero;
-    }
-
-    /**
-     * @param numero the numero to set
-     */
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    /**
-     * @return the complemento
-     */
-    public String getComplemento() {
-        return complemento;
-    }
-
-    /**
-     * @param complemento the complemento to set
-     */
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    /**
-     * @return the bairro
-     */
-    public String getBairro() {
-        return bairro;
-    }
-
-    /**
-     * @param bairro the bairro to set
-     */
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    /**
-     * @return the estado
-     */
-    public Estado getEstado() {
-        return estado;
-    }
-
-    /**
-     * @param estado the estado to set
-     */
-    public void setEstado(Estado estado) {
-        this.estado = estado;
-    }
-
-    /**
-     * @return the cidade
-     */
-    public Cidade getCidade() {
-        return cidade;
-    }
-
-    /**
-     * @param cidade the cidade to set
-     */
-    public void setCidade(Cidade cidade) {
-        this.cidade = cidade;
-    }
-
-    /**
-     * @return the fone1
-     */
-    public String getFone1() {
-        return fone1;
-    }
-
-    /**
-     * @param fone1 the fone1 to set
-     */
-    public void setFone1(String fone1) {
-        this.fone1 = fone1;
-    }
-
-    /**
-     * @return the fone2
-     */
-    public String getFone2() {
-        return fone2;
-    }
-
-    /**
-     * @param fone2 the fone2 to set
-     */
-    public void setFone2(String fone2) {
-        this.fone2 = fone2;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * @return the atributoPadrao
-     */
-    public AtributoPadrao getAtributoPadrao() {
-        return atributoPadrao;
-    }
-
-    /**
-     * @param atributoPadrao the atributoPadrao to set
-     */
-    public void setAtributoPadrao(AtributoPadrao atributoPadrao) {
-        this.atributoPadrao = atributoPadrao;
-    }
-
-    /**
-     * @return the tipoRegime
-     */
     public DominioRegime getTipoRegime() {
         return tipoRegime;
     }
 
-    /**
-     * @param tipoRegime the tipoRegime to set
-     */
     public void setTipoRegime(DominioRegime tipoRegime) {
         this.tipoRegime = tipoRegime;
     }
+
+    public DominioTipoPessoa getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public void setTipoPessoa(DominioTipoPessoa tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public EnderecoPadrao getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(EnderecoPadrao endereco) {
+        this.endereco = endereco;
+    }
+
+    public AtributoPadrao getAtributoPadrao() {
+        return atributoPadrao;
+    }
+
+    public void setAtributoPadrao(AtributoPadrao atributoPadrao) {
+        this.atributoPadrao = atributoPadrao;
+    }
+   
 
 }
