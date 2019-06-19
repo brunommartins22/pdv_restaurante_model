8971e8c045605da6192d71f2fcb876351346dc71
joao pamplona <joaopweb@gmail.com>
2019-06-19 09:46:13 -0300

    regra ncm

--- src/main/java/br/com/interagese/padrao/services/RegraNcmService.java
+++ src/main/java/br/com/interagese/padrao/services/RegraNcmService.java
@@ -12,6 +12,7 @@ import br.com.interagese.syscontabil.domains.DominioRegime;
 import br.com.interagese.syscontabil.dto.RegraNcmDto;
 import br.com.interagese.syscontabil.models.RegraNcm;
 import br.com.interagese.syscontabil.models.RegraNcmPK;
+import br.com.interagese.syscontabil.models.TributoFederal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -25,92 +26,116 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegraNcmService extends PadraoService<RegraNcmDto>{
    
-    @Override
-    public List<RegraNcmDto> findRange(String complementoConsulta, int apartirDe, int quantidade) {
+    public List<RegraNcmDto> findRangeDto(String complementoConsulta, int apartirDe, int quantidade) {
 
+        String consultaId = "SELECT distinct(o.id.codigoNcm) from RegraNcm o ";
         String consulta = "SELECT o from RegraNcm o ";
+        
         String consultaSQL = "";
         consultaSQL = getWhere(complementoConsulta);
 
         if (complementoConsulta != null && !complementoConsulta.trim().equals("")) {
-            consulta += " where " + consultaSQL;
+            consultaId += " where " + consultaSQL;
         }
 
         boolean existeAtributoPadrao = Utils.existeAtributoPadraoByClass(RegraNcm.class);
 
 //        String sqlComplementar = "";
         if (existeAtributoPadrao) {
-            consulta += (consulta.contains("where") ? " and " : " where ") + " o.atributoPadrao.dominioEvento <>  3";
+            consultaId += (consulta.contains("where") ? " and " : " where ") + " o.atributoPadrao.dominioEvento <>  3";
         }
         
-        consulta += " order by o.id.codigoNcm";
+        consultaId += " order by o.id.codigoNcm";
         
-        List<RegraNcm> listaResultNcm = em.createQuery(consulta)
-                .setMaxResults(quantidade*3). // cada registro da regra possui 3 registros da tabela
-                setFirstResult(apartirDe*3). // cada registro da regra possui 3 registros da tabela
+        List<String> listaCodigoNcm = em.createQuery(consultaId)
+                .setMaxResults(quantidade).
+                setFirstResult(apartirDe).
                 getResultList();
         
-        return getListaAgrupada(listaResultNcm);
+        List<RegraNcmDto> listaDto = new ArrayList<>(); 
+        
+        for(String ncm: listaCodigoNcm){
+        
+            List<RegraNcm> listaResultNcm = em.createQuery(consulta + " where o.id.codigoNcm = (:ncm) and o.atributoPadrao.dominioEvento <> 3 order by o.id.codigoNcm")
+                .setParameter("ncm", ncm)
+                .setMaxResults(3) // cada registro da regra possui 3 registros da tabela
+                .getResultList();
+            
+            listaDto.add(getDto(listaResultNcm));
+            
+        }
+       
+        return listaDto;
     }
 
-    @Override
-    public List<RegraNcmDto> search(Map filtros, int apartirDe, int quantidade) {
+    public List<RegraNcmDto> searchDto(Map filtros, int apartirDe, int quantidade) {
 
+        String consultaId = "SELECT distinct(o.id.codigoNcm) from RegraNcm o ";
         String consulta = "SELECT o from RegraNcm o ";
+        
         String consultaSQL = "";
         consultaSQL = getWhere(filtros);
 
         if (filtros != null && !filtros.isEmpty()) {
-            consulta += " where " + consultaSQL + " ";
+            consultaId += " where " + consultaSQL + " ";
         }
 
-        consulta += " order by o.id.codigoNcm";
+        consultaId += " order by o.id.codigoNcm";
         
-        List<RegraNcm> listaResultNcm = em.createQuery(consulta)
-                .setMaxResults(quantidade*3). // cada registro da regra possui 3 registros da tabela
-                setFirstResult(apartirDe*3). // cada registro da regra possui 3 registros da tabela
+        List<String> listaCodigoNcm = em.createQuery(consultaId)
+                .setMaxResults(quantidade).
+                setFirstResult(apartirDe).
                 getResultList();
         
-        return getListaAgrupada(listaResultNcm);
+        List<RegraNcmDto> listaDto = new ArrayList<>(); 
+        
+        for(String ncm: listaCodigoNcm){
+        
+            List<RegraNcm> listaResultNcm = em.createQuery(consulta + " where o.id.codigoNcm = (:ncm) and o.atributoPadrao.dominioEvento <> 3 order by o.id.codigoNcm")
+                .setParameter("ncm", ncm)
+                .setMaxResults(3) // cada registro da regra possui 3 registros da tabela
+                .getResultList();
+            
+            listaDto.add(getDto(listaResultNcm));
+            
+        }
+       
+        return listaDto;
     }
 
-    @Override
-    public String count(String complementoConsulta) {
+    public String countDto(String complementoConsulta) {
         String consultaSQL = "";
         consultaSQL = getWhere(complementoConsulta);
 
-        String consulta = "SELECT count(o) from RegraNcm o ";
+        String consulta = "SELECT count(distinct o.id.codigoNcm) from RegraNcm o ";
 
         if (complementoConsulta != null && !complementoConsulta.trim().equals("")) {
             consulta += " where " + consultaSQL;
         }
 
         Long l = (Long) em.createQuery(consulta).getSingleResult();
+        
         if (l == null) {
             l = 0L;
-        } else {
-            l = l/3; // um registro para cada grupo de 3
         }
 
         return l.toString();
     }
     
-    @Override
-    public RegraNcmDto findById(Object id) {
-        String consulta = "SELECT o from RegraNcm o where o.id.codigoNcm = :id";
-        
-        List<RegraNcm> listaResultNcm = em.createQuery(consulta)
-                .setParameter("id", id.toString())
+    public RegraNcmDto findByIdDto(Object id) {
+        List<RegraNcm> listaResultNcm = em.createQuery("SELECT o from RegraNcm o where o.id.codigoNcm = (:ncm) and o.atributoPadrao.dominioEvento <> 3 order by o.id.codigoNcm")
+                .setParameter("ncm", id.toString())
+                .setMaxResults(3) // cada registro da regra possui 3 registros da tabela
                 .getResultList();
-        if(!getListaAgrupada(listaResultNcm).isEmpty()){
-            return getListaAgrupada(listaResultNcm).get(0);
+            
+        if(!listaResultNcm.isEmpty()){
+            return getDto(listaResultNcm);
         } else {
             return null;
         }
     }
     
-    @Override
-    public RegraNcmDto create(RegraNcmDto obj) throws Exception {
+    public RegraNcmDto createDto(RegraNcmDto obj) throws Exception {
         try {
             setID(obj);
 
@@ -119,76 +144,163 @@ public class RegraNcmService extends PadraoService<RegraNcmDto>{
                 atributoPadrao.setDataAlteracao(new Date());
             }
             
-            obj.getRegraNcmLucroPresumido().setId(new RegraNcmPK());
-            obj.getRegraNcmLucroPresumido().getId().setCodigoNcm(obj.getId());
-            obj.getRegraNcmLucroPresumido().getId().setRegimeTributarioId(DominioRegime.LUCROPRESUMIDO);
-            obj.getRegraNcmLucroPresumido().setAtributoPadrao(obj.getAtributoPadrao());
+            if (obj.getRegraNcmLucroPresumido().getId() != null) {
+                // obj.getRegraNcmLucroPresumido().setId(new RegraNcmPK());
+                // obj.getRegraNcmLucroPresumido().getId().setCodigoNcm(obj.getId());
+                obj.getRegraNcmLucroPresumido().getId().setRegimeTributarioId(DominioRegime.LUCROPRESUMIDO);
+                obj.getRegraNcmLucroPresumido().setAtributoPadrao(obj.getAtributoPadrao());
+                em.persist(obj.getRegraNcmLucroPresumido());
+            }
             
-            obj.getRegraNcmLucroReal().setId(new RegraNcmPK());
-            obj.getRegraNcmLucroReal().getId().setCodigoNcm(obj.getId());
-            obj.getRegraNcmLucroReal().getId().setRegimeTributarioId(DominioRegime.LUCROREAL);
-            obj.getRegraNcmLucroReal().setAtributoPadrao(obj.getAtributoPadrao());
+            if(obj.getRegraNcmLucroReal().getId() != null){
+                // obj.getRegraNcmLucroReal().setId(new RegraNcmPK());
+                // obj.getRegraNcmLucroReal().getId().setCodigoNcm(obj.getId());
+                obj.getRegraNcmLucroReal().getId().setRegimeTributarioId(DominioRegime.LUCROREAL);
+                obj.getRegraNcmLucroReal().setAtributoPadrao(obj.getAtributoPadrao());
+                em.persist(obj.getRegraNcmLucroReal());
+            }
             
-            obj.getRegraNcmSimplesNacional().setId(new RegraNcmPK());
-            obj.getRegraNcmSimplesNacional().getId().setCodigoNcm(obj.getId());
-            obj.getRegraNcmSimplesNacional().getId().setRegimeTributarioId(DominioRegime.SIMPLESNACIONAL);
-            obj.getRegraNcmSimplesNacional().setAtributoPadrao(obj.getAtributoPadrao());
-            
-            em.persist(obj.getRegraNcmLucroPresumido());
-            em.persist(obj.getRegraNcmLucroReal());
-            em.persist(obj.getRegraNcmSimplesNacional());
-
+            if(obj.getRegraNcmSimplesNacional().getId() != null){
+                // obj.getRegraNcmSimplesNacional().setId(new RegraNcmPK());
+                // obj.getRegraNcmSimplesNacional().getId().setCodigoNcm(obj.getId());
+                obj.getRegraNcmSimplesNacional().getId().setRegimeTributarioId(DominioRegime.SIMPLESNACIONAL);
+                obj.getRegraNcmSimplesNacional().setAtributoPadrao(obj.getAtributoPadrao());
+                em.persist(obj.getRegraNcmSimplesNacional());
+            }
+        
             return obj;
         } catch (IllegalArgumentException ex) {
             return null;
         }
     }
-
-    @Override
-    public RegraNcmDto update(RegraNcmDto obj) throws Exception {
+    
+    public RegraNcmDto updateDto(RegraNcmDto obj) throws Exception {
 
         AtributoPadrao atributoPadrao = Utils.getAtributoPadraoByObject(obj);
         if (atributoPadrao != null) {
             atributoPadrao.setDataAlteracao(new Date());
         }
-
-        em.merge(obj.getRegraNcmLucroPresumido());
-        em.merge(obj.getRegraNcmLucroReal());
-        em.merge(obj.getRegraNcmSimplesNacional());
+        
+        if(obj.getRegraNcmLucroPresumido().getId() != null && obj.getRegraNcmLucroPresumido().getId().getCodigoNcm() != null){
+            obj.getRegraNcmLucroPresumido().setAtributoPadrao(obj.getAtributoPadrao());
+            em.merge(obj.getRegraNcmLucroPresumido());
+        }
+        
+        if(obj.getRegraNcmLucroReal().getId() != null && obj.getRegraNcmLucroReal().getId().getCodigoNcm() != null){
+            obj.getRegraNcmLucroReal().setAtributoPadrao(obj.getAtributoPadrao());
+            em.merge(obj.getRegraNcmLucroReal());
+        }
+        
+        if(obj.getRegraNcmSimplesNacional().getId() != null && obj.getRegraNcmSimplesNacional().getId().getCodigoNcm() != null){
+            obj.getRegraNcmSimplesNacional().setAtributoPadrao(obj.getAtributoPadrao());
+            em.merge(obj.getRegraNcmSimplesNacional());
+        }
+        
         return obj;
+    }
+    
+    public RegraNcmDto deleteDto(RegraNcmDto obj) throws Exception {
+
+        AtributoPadrao atributoPadrao = Utils.getAtributoPadraoByObject(obj);
+        if (atributoPadrao != null) {
+            atributoPadrao.setDataAlteracao(new Date());
+        }
+        
+        if(obj.getRegraNcmLucroPresumido().getId() != null){
+            obj.getRegraNcmLucroPresumido().setAtributoPadrao(obj.getAtributoPadrao());
+            em.merge(obj.getRegraNcmLucroPresumido());
+        }
+        
+        if(obj.getRegraNcmLucroReal().getId() != null){
+            obj.getRegraNcmLucroReal().setAtributoPadrao(obj.getAtributoPadrao());
+            em.merge(obj.getRegraNcmLucroReal());
+        }
+        
+        if(obj.getRegraNcmSimplesNacional().getId() != null){
+            obj.getRegraNcmSimplesNacional().setAtributoPadrao(obj.getAtributoPadrao());
+            em.merge(obj.getRegraNcmSimplesNacional());
+        }
+        
+        return obj;
+    }
+    
+    public RegraNcmDto getDto(List<RegraNcm> listaResultNcm){
+        
+        if(listaResultNcm.size() > 0){
+            
+            RegraNcmDto dto = new RegraNcmDto();
+            dto.setRegraNcmLucroPresumido(new RegraNcm());
+            dto.getRegraNcmLucroPresumido().setId(new RegraNcmPK());
+            dto.getRegraNcmLucroPresumido().setTributoFederal(new TributoFederal());
+            dto.setRegraNcmLucroReal(new RegraNcm());
+            dto.getRegraNcmLucroReal().setId(new RegraNcmPK());
+            dto.getRegraNcmLucroReal().setTributoFederal(new TributoFederal());
+            dto.setRegraNcmSimplesNacional(new RegraNcm());
+            dto.getRegraNcmSimplesNacional().setId(new RegraNcmPK());
+            dto.getRegraNcmSimplesNacional().setTributoFederal(new TributoFederal());
+       
+            for( RegraNcm regraNcm: listaResultNcm){
+
+                dto.setId(regraNcm.getId().getCodigoNcm());
+                        
+                if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.LUCROPRESUMIDO)){
+                    dto.setRegraNcmLucroPresumido(regraNcm);
+                    dto.setSituacaoLucroPresumido("Configurado");
+                }
+                if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.LUCROREAL)){
+                    dto.setRegraNcmLucroReal(regraNcm);
+                    dto.setSituacaoLucroReal("Configurado");
+                }
+                if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.SIMPLESNACIONAL)){
+                    dto.setRegraNcmSimplesNacional(regraNcm);
+                    dto.setSituacaoSimplesNacional("Configurado");
+                }
+                
+            }
+        
+            return dto;
+        } else {
+            return null;
+        }
+        
     }
     
     public List<RegraNcmDto> getListaAgrupada(List<RegraNcm> listaResultNcm){
         
         List<RegraNcmDto> listaResult = new ArrayList<RegraNcmDto>();
         
-        RegraNcmDto dto = new RegraNcmDto();
+        if(listaResultNcm.size() > 0){    
+            
+            RegraNcmDto dto = new RegraNcmDto();
+
+            for( RegraNcm regraNcm: listaResultNcm){
+
+                if(dto.getId() == null){
+                    dto.setId(regraNcm.getId().getCodigoNcm());
+                }
+
+                if(!regraNcm.getId().getCodigoNcm().equals(dto.getId())){
+                    listaResult.add(dto);
+                    dto = new RegraNcmDto();
+                    dto.setId(regraNcm.getId().getCodigoNcm());
+                }
+
+                if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.LUCROPRESUMIDO)){
+                    dto.setRegraNcmLucroPresumido(regraNcm);
+                }
+                if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.LUCROREAL)){
+                    dto.setRegraNcmLucroReal(regraNcm);
+                }
+                if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.SIMPLESNACIONAL)){
+                    dto.setRegraNcmSimplesNacional(regraNcm);
+                }
+            }
+
+            listaResult.add(dto); // adiciona o ultimo registro
         
-        for( RegraNcm regraNcm: listaResultNcm){
-            
-            if(dto.getId() == null){
-                dto.setId(regraNcm.getId().getCodigoNcm());
-            }
-            
-            if(!regraNcm.getId().getCodigoNcm().equals(dto.getId())){
-                listaResult.add(dto);
-                dto = new RegraNcmDto();
-                dto.setId(regraNcm.getId().getCodigoNcm());
-            }
-            
-            if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.LUCROPRESUMIDO)){
-                dto.setRegraNcmLucroPresumido(regraNcm);
-            }
-            if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.LUCROREAL)){
-                dto.setRegraNcmLucroReal(regraNcm);
-            }
-            if(regraNcm.getId().getRegimeTributarioId().equals(DominioRegime.SIMPLESNACIONAL)){
-                dto.setRegraNcmSimplesNacional(regraNcm);
-            }
         }
         
-        listaResult.add(dto); // adiciona o ultimo registro
-        
         return listaResult;
+        
     }
 }
