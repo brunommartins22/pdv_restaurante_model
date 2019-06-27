782c2a12c56759d74c34cb000234b9a54aab2d88
josev.junior <josev.junior@outlook.com>
2019-06-27 16:19:55 -0300

    Alterações:  - Correção no tempo da sessão

--- src/main/java/br/com/interagese/CrudApplication.java
+++ src/main/java/br/com/interagese/CrudApplication.java
@@ -1,6 +1,6 @@
 package br.com.interagese;
 
-import br.com.interagese.padrao.rest.util.MultiDatasourceConfiguration;
+import br.com.interagese.padrao.rest.util.configuration.MultiDatasourceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
