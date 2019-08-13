e4a84f73a3c1d4707361bd322f3d5e1362a963c7
Bruno <bruno76705@gmail.com>
2019-08-13 11:40:46 -0300

    Remove ClienteId, CenarioID

--- src/main/java/br/com/interagese/padrao/services/RegraNcmService.java
+++ src/main/java/br/com/interagese/padrao/services/RegraNcmService.java
@@ -7,11 +7,9 @@ package br.com.interagese.padrao.services;
 
 import br.com.interagese.padrao.rest.util.PadraoService;
 
-import br.com.interagese.syscontabil.models.Cenario;
 import br.com.interagese.syscontabil.models.RegraNcm;
 import java.util.List;
 import javax.persistence.TypedQuery;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,39 +19,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegraNcmService extends PadraoService<RegraNcm> {
 
-    @Autowired
-    private CenarioService cenarioService;
-
     @Override
     public List<RegraNcm> findRange(String complementoConsulta, int apartirDe, int quantidade, String ordernacao) {
         List<RegraNcm> result = super.findRange(complementoConsulta, apartirDe, quantidade, ordernacao); //To change body of generated methods, choose Tools | Templates.
 
         if (!result.isEmpty()) {
-            for (RegraNcm rt : result) {
+            result.forEach((rt) -> {
                 rt.setNomeRegime(rt.getRegimeTributario().getDescricao());
-                String nome = "";
-                if (rt.getCenarioId() != null) {
-                    Cenario cenario = cenarioService.findById(rt.getCenarioId());
-                    nome = cenario != null ? cenario.getNomeCenario() : "Todos";
-                } else {
-                    nome = "Todos";
-                }
-                rt.setNomeCenario(nome);
-            }
+            });
         }
-        
+
         return result;
     }
 
+    /**
+     * CORRIGIR SQL DE CONSULTA
+     */
     public RegraNcm loadRegraNcm(String codigoNcm, String regime) {
 
         String sql = "select * from syscontabil.regra_ncm where codigo_ncm = :ncm and regime_tributario_id = :regime";
 
-        TypedQuery<RegraNcm> lista = (TypedQuery<RegraNcm>) em.createNativeQuery(sql,RegraNcm.class)
+        TypedQuery<RegraNcm> lista = (TypedQuery<RegraNcm>) em.createNativeQuery(sql, RegraNcm.class)
                 .setParameter("ncm", codigoNcm)
                 .setParameter("regime", regime);
 
         return lista.getResultList().isEmpty() ? null : lista.getSingleResult();
     }
-    
+
 }
--- src/main/java/br/com/interagese/padrao/services/RegraRegimeService.java
+++ src/main/java/br/com/interagese/padrao/services/RegraRegimeService.java
@@ -8,10 +8,8 @@ package br.com.interagese.padrao.services;
 import br.com.interagese.erplibrary.Utils;
 import br.com.interagese.syscontabil.models.RegraRegimeTributario;
 import br.com.interagese.padrao.rest.util.PadraoService;
-import br.com.interagese.syscontabil.models.Cenario;
 import java.util.List;
 import javax.persistence.TypedQuery;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,15 +18,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RegraRegimeService extends PadraoService<RegraRegimeTributario> {
-    
-    @Autowired
-    private CenarioService cenarioService;
 
     //************************ create business rules ***************************
     @Override
     public String getWhere(String complementoConsulta) {
         String consultaSQL = "";
-        
+
         if (complementoConsulta != null && !complementoConsulta.trim().equals("")) {
             if (Utils.somenteNumeros(complementoConsulta)) {
                 consultaSQL = "o.id = '" + complementoConsulta;
@@ -36,56 +31,39 @@ public class RegraRegimeService extends PadraoService<RegraRegimeTributario> {
                 consultaSQL = "o.regime_tributario = '" + complementoConsulta + "' ";
             }
         }
-        
+
         return consultaSQL;
     }
-    
+
     @Override
     public RegraRegimeTributario findById(Object id) {
         RegraRegimeTributario o = (RegraRegimeTributario) em.createQuery("SELECT o from RegraRegimeTributario o where o.id = :id ").setParameter("id", Long.parseLong(id.toString())).getSingleResult();
-        
+
         return o;
     }
-    
+
+     /**
+     * CORRIGIR SQL DE CONSULTA
+     */
     public RegraRegimeTributario loadRegraRegimeTributario(String regime) {
         String sql = "select * from syscontabil.regra_regime_tributario where regime_tributario_id = :regime";
-        
+
         TypedQuery<RegraRegimeTributario> lista = (TypedQuery<RegraRegimeTributario>) em.createNativeQuery(sql, RegraRegimeTributario.class).setParameter("regime", regime);
-        
+
         return lista.getResultList().isEmpty() ? null : lista.getSingleResult();
     }
 
-//    @Override
-//    public List<RegraRegimeTributario> findRange(String complementoConsulta, int apartirDe, int quantidade, String ordernacao) {
-//
-//        List<RegraRegimeTributario> result = super.findRange(complementoConsulta, apartirDe, quantidade, ordernacao);
-//
-//        result.forEach((t) -> {
-//            t.setNomeRegime(t.getRegimeTributario().getDescricao());
-//            t.setNomeCenario(t.getCenarioId() == null ? "Todos" : cenarioService.findById(t.getCenarioId()));
-//        });
-//
-//        return result;
-//    }
     @Override
     public List<RegraRegimeTributario> findRange(String complementoConsulta, int apartirDe, int quantidade, String ordernacao) {
         List<RegraRegimeTributario> result = super.findRange(complementoConsulta, apartirDe, quantidade, ordernacao); //To change body of generated methods, choose Tools | Templates.
 
         if (!result.isEmpty()) {
-            for (RegraRegimeTributario rt : result) {
+            result.forEach((rt) -> {
                 rt.setNomeRegime(rt.getRegimeTributario().getDescricao());
-                String nome = "";
-                if (rt.getCenarioId() != null) {
-                    Cenario cenario = cenarioService.findById(rt.getCenarioId());
-                    nome = cenario != null ? cenario.getNomeCenario() : "Todos";
-                } else {
-                    nome = "Todos";
-                }
-                rt.setNomeCenario(nome);
-            }
+            });
         }
-        
+
         return result;
     }
-    
+
 }
